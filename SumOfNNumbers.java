77b0fd3ac98df4136f163577577e20567cf24bec
Prashanth Charla <prashanthcharla1234@gmail.com>
2022-10-29 00:02:03 +0530

    Sum of N numbers using recursion

---
+++ SumOfNNumbers.java
@@ -0,0 +1,11 @@
+public class SumOfNNumbers {
+    public static int sum(int n) {
+        if(n == 1) return n;
+
+        return n + sum(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+}
